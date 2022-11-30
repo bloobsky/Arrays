ad1525a27671940908b79ccd11f315ef425d4330
bloobsky <email@example.com>
2022-11-30 21:48:43 +0000

    setObj, getObj, getSize functions added.

--- demo/src/main/java/com/example/GenericArrayList.java
+++ demo/src/main/java/com/example/GenericArrayList.java
@@ -13,32 +13,53 @@ public class GenericArrayList<T> implements IList<T>{
     }
 
     @Override
-    public void add(T element) {
+    public void add(T elem) {
         T[] temporary = (T[]) new Object[buffer.length+1]; // increasing value
         int i = 0;
 
         for (i = 0; i<buffer.length; i++) {
             temporary[i] = buffer[i];
         }
-        temporary[i] = element;
+        temporary[i] = elem;
         buffer = temporary;
 
     }
     
     @Override
-    public void add(int index, T element) throws IndexOutOfBoundsException{
+    public void add(int index, T elem) throws IndexOutOfBoundsException{
         IndexCheck(index);
         T[] temporary = (T[]) new Object[buffer.length+1];
 
         for(int i = temporary.length-1; i>=0; i--) {
             if (i == index) {
-                temporary[i] = element;
+                temporary[i] = elem;
                 i--;
                 if(i<0) break; // skipping
             }
             temporary[i] = buffer[i-1];
         }
         buffer = temporary;
+    }
+
+    @Override
+    // set obj in array
+    public T setObj(int index, T elem) {
+        T previous = buffer[index];
+        buffer[index] = elem;
+        return previous;
+    }
+    
+    @Override
+    // get obj from array
+    public getObj(int index) throws IndexOutOfBoundsException {
+        IndexCheck(index);
+        return buffer[index];
+    }
+
+    @Override
+    // get size of array function
+    public int getSize() {
+        return buffer.length;
     }
 
 }
