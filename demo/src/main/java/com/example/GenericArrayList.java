7685beaf9a63758e837df8c6463ddf19e394655a
bloobsky <email@example.com>
2022-12-03 13:18:31 +0000

    IStack finished.

--- demo/src/main/java/com/example/GenericArrayList.java
+++ demo/src/main/java/com/example/GenericArrayList.java
@@ -27,39 +27,118 @@ public class GenericArrayList<T> implements IList<T>{
     
     @Override
     public void add(int index, T elem) throws IndexOutOfBoundsException{
-        IndexCheck(index);
-        T[] temporary = (T[]) new Object[buffer.length+1];
+        if (index > buffer.length) throw new IndexOutOfBoundsException("ERROR: Index of range.");
 
-        for(int i = temporary.length-1; i>=0; i--) {
+        T[] temporary = (T[]) new Object[buffer.length+1];
+        int indexTemp = 0;
+        for(int i = 0; i <= buffer.length; i++) {
             if (i == index) {
-                temporary[i] = elem;
-                i--;
-                if(i<0) break; // skipping
+                temporary[indexTemp] = elem;
+                indexTemp++;
             }
-            temporary[i] = buffer[i-1];
+
+            if(i == buffer.length) break;
+                temporary[indexTemp] = buffer[i];
+                indexTemp++;
+                         
         }
         buffer = temporary;
+        
     }
 
     @Override
     // set obj in array
-    public T setObj(int index, T elem) {
+    public T set(int index, T elem) {
         T previous = buffer[index];
         buffer[index] = elem;
         return previous;
     }
     
     @Override
-    // get obj from array
-    public getObj(int index) throws IndexOutOfBoundsException {
-        IndexCheck(index);
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index > buffer.length) throw new IndexOutOfBoundsException("ERROR: Index of range.");
         return buffer[index];
     }
 
     @Override
     // get size of array function
-    public int getSize() {
+    public int size() {
         return buffer.length;
+    }
+
+    @Override
+    public T remove(int index) throws IndexOutOfBoundsException {
+        if (index > buffer.length) throw new IndexOutOfBoundsException("ERROR: Index of range.");
+
+        T[] temporary = (T[]) new Object[buffer.length -1];
+        T entry = buffer[index];
+        int indexTemp = 0;
+
+        for (int i = 0; i < buffer.length; i++) {
+            if (i == index) i++;
+            if (i == buffer.length) break;
+
+            temporary[indexTemp] = buffer[i];
+            indexTemp++;
+        }
+        buffer = temporary;
+        return entry;
+
+    }
+
+    @Override
+    public boolean remove(T elem) {
+        T[] temporary = (T[]) new Object[buffer.length - 1];
+        int indexTemp = 0;
+        boolean value = false;
+
+        for (int i = 0; i < buffer.length; i++) {
+            if(buffer[i] == elem) {
+                value = true;
+                i++;
+            }
+            if (i == buffer.length || indexTemp == temporary.length) break;
+
+            temporary[indexTemp] = buffer[i];
+            indexTemp++;
+
+            if(value) buffer = temporary;
+        }
+        return value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return buffer.length == 0;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        for (int i = buffer.length -1; i >=0; i--) {
+            if (buffer[i] == element) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new GenericArrayListIterator();
+    }
+
+    class GenericArrayListIterator implements Iterator<T> {
+
+        private int val = 0;
+
+        @Override
+        public boolean hasNext() {
+            return val < buffer.length;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return buffer[val++];
+        }
     }
 
 }
