a5ad7d1bd943262f70d86e8faaadfd72ec700291
bloobsky <email@example.com>
2022-12-03 22:30:28 +0000

    Half Test for GenericArray added.

---
+++ demo/src/test/java/com/example/GenericArrayListTest.java
@@ -0,0 +1,104 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class GenericArrayListTest {
+    
+    //setup config
+    GenericArrayList<String> gList;
+
+    @BeforeEach
+    void setup() {
+        gList = new GenericArrayList<>();
+    }
+
+    
+    @Test
+    void add_3Elements_ReturnSize() {
+        // checking 3 elements and if they return size of array
+        gList.add("First Elem");
+        assertEquals(1, gList.size());
+
+        gList.add("Second Elem");
+        assertEquals(2, gList.size());
+
+        gList.add("Third Elem");
+        assertEquals(3, gList.size());
+    }
+
+    @Test 
+    // checking if data is moving with array
+    void add_3Elements_CorrectIndex() {
+        gList.add(0, "First Elem");
+        gList.add(0, "Second Elem");
+        gList.add(2, "Third Elem");
+
+        assertEquals("Second Elem", gList.get(0));
+        assertEquals("First Elem", gList.get(1));
+        assertEquals("Third Elem", gList.get(2));
+
+    }
+
+    @Test
+    // checking throw exception
+    void throw_exception() {
+        assertThrows ( IndexOutOfBoundsException.class, () -> {gList.add(100, "Hundred");});
+    }
+
+    @Test
+    //adding 4 values and checking  if set correctly
+    void set_5Val_CheckSet() {
+        gList.add("V");
+        gList.add("Va");
+        gList.add("Val");
+        gList.add("Valu");
+        gList.add("Value");
+
+        gList.set(0, "V");
+        gList.set(0, "Va");
+        gList.set(0, "Val");
+        gList.set(0, "Valu");
+        gList.set(0, "Value");
+    }
+
+    @Test
+    // set with throw exception 
+    void set_throw_exception() {
+        assertThrows(IndexOutOfBoundsException.class, () -> { gList.set(5, "5th");});
+    }
+
+
+
+    @Test
+    //adding and checking if the same value returned
+    void get_AddAndGet_CheckReturn() {
+    
+        gList.add("This is what I am looking for");
+        assertEquals("This is what I am looking for", gList.get(0));
+    }
+
+    @Test
+    // get value outside of index range
+    void get_throw_exception () {
+        gList.add("ERROR");
+        assertThrows(IndexOutOfBoundsException.class, () -> { gList.get(5);});
+    }
+
+    @Test
+    // checking if correct entry in correct place
+    void contains_3Elements_CheckPresence() {
+        gList.add("A");
+        gList.add("B");
+        gList.add("C");
+
+        assertEquals(true, gList.contains("A"));
+        assertEquals(true, gList.contains("B"));
+        assertEquals(true, gList.contains("C"));
+    }
+
+}
